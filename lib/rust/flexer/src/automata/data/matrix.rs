098a3fca59c4d03dbb4000158d9ecde6eb5c8bd0
Ara Adkins <me@ara.io>
2020-10-14 09:59:43 +0100

    Add matrix tests

--- lib/rust/flexer/src/automata/data/matrix.rs
+++ lib/rust/flexer/src/automata/data/matrix.rs
@@ -45,7 +45,7 @@ impl<T:Copy> Matrix<T> {
 }
 
 impl<T:Default> Matrix<T> {
-    /// Constructs a matrix with the dimensions given by `rows` and `columns`.
+    /// Construct a matrix with the dimensions given by `rows` and `columns`.
     pub fn new(rows:usize, columns:usize) -> Self {
         let mut matrix = Vec::with_capacity(rows*columns);
         for _ in 0..matrix.capacity() {
@@ -54,12 +54,24 @@ impl<T:Default> Matrix<T> {
         Self{rows,columns,matrix}
     }
 
-    /// Adds a new row to the matrix `self`, filled with default values.
+    /// Add a new row to the matrix `self`, filled with default values.
     pub fn new_row(&mut self) {
         for _ in 0..self.columns {
             self.matrix.push(default());
         }
         self.rows += 1;
+    }
+
+    /// Add a new column to the matrix `self`, filled with default values.
+    ///
+    /// Note that this is an _expensive_ operation that requires moving potentially very large
+    /// allocations around.
+    pub fn new_column(&mut self) {
+        for n in 0..self.rows {
+            let index = self.columns*n + self.columns+n;
+            self.matrix.insert(index,default());
+        }
+        self.columns += 1;
     }
 }
 
@@ -76,5 +88,95 @@ impl<T> Index<(usize,usize)> for Matrix<T> {
 impl<T> IndexMut<(usize,usize)> for Matrix<T> {
     fn index_mut(&mut self, index:(usize,usize)) -> &mut T {
         &mut self.matrix[index.0*self.columns+index.1]
+    }
+}
+
+
+
+// =============
+// === Tests ===
+// =============
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_size() {
+        let default = Matrix::<usize>::default();
+        assert_eq!(default.rows,0);
+        assert_eq!(default.columns,0);
+    }
+
+    #[test]
+    fn construct_with_dimensions() {
+        let matrix = Matrix::<usize>::new(5, 10);
+        assert_eq!(matrix.rows,5);
+        assert_eq!(matrix.columns,10);
+    }
+
+    #[test]
+    fn add_row() {
+        let mut matrix = Matrix::<usize>::new(2, 2);
+        matrix[(0,0)] = 1;
+        matrix[(0,1)] = 2;
+        matrix[(1,0)] = 3;
+        matrix[(1,1)] = 4;
+        assert_eq!(matrix.rows,2);
+        assert_eq!(matrix.columns,2);
+        matrix.new_row();
+        assert_eq!(matrix.rows,3);
+        assert_eq!(matrix.columns,2);
+        assert_eq!(matrix[(0,0)],1);
+        assert_eq!(matrix[(0,1)],2);
+        assert_eq!(matrix[(1,0)],3);
+        assert_eq!(matrix[(1,1)],4);
+        assert_eq!(matrix[(2,0)],0);
+        assert_eq!(matrix[(2,1)],0);
+    }
+
+    #[test]
+    fn add_column() {
+        let mut matrix = Matrix::<usize>::new(2,2);
+        matrix[(0,0)] = 1;
+        matrix[(0,1)] = 2;
+        matrix[(1,0)] = 3;
+        matrix[(1,1)] = 4;
+        assert_eq!(matrix.rows,2);
+        assert_eq!(matrix.columns,2);
+        matrix.new_column();
+        assert_eq!(matrix.rows,2);
+        assert_eq!(matrix.columns,3);
+        assert_eq!(matrix[(0,0)],1);
+        assert_eq!(matrix[(0,1)],2);
+        assert_eq!(matrix[(1,0)],3);
+        assert_eq!(matrix[(1,1)],4);
+        assert_eq!(matrix[(0,2)],0);
+        assert_eq!(matrix[(1,2)],0);
+    }
+
+    #[test]
+    fn row_column_indexing() {
+        let mut matrix = Matrix::<usize>::new(2,2);
+        matrix[(0,0)] = 1;
+        matrix[(0,1)] = 2;
+        matrix[(1,0)] = 3;
+        matrix[(1,1)] = 4;
+        let mut output = Vec::default();
+        for row in 0..2 {
+            for col in 0..2 {
+                output.push(matrix[(row,col)]);
+            }
+        }
+        assert_eq!(output,vec![1,2,3,4]);
+    }
+
+    #[test]
+    fn safe_indexing() {
+        let matrix         = Matrix::<usize>::new(2,2);
+        let exists         = matrix.safe_index(0,0);
+        let does_not_exist = matrix.safe_index(3,0);
+        assert_eq!(exists,Some(0));
+        assert_eq!(does_not_exist,None);
     }
 }
